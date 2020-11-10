92ea7cebf054413d3b44c33d7337537fce889b21
rockenbachz <vrockenbach77@gmail.com>
2020-11-10 20:55:01 -0300

    course app finished

--- src/main.js
+++ src/main.js
@@ -1,3 +1,103 @@
+import api from './api';
+
+class Repo{
+  constructor() {
+    this.repositories = [];
+
+    this.formEl = document.getElementById('repo-form');
+
+    this.inputEl = document.querySelector('input[name=repository]');
+
+    this.listEl = document.getElementById('repo-list');
+    this.registerHandlers();
+  }
+
+  registerHandlers() {
+    this.formEl.onsubmit = event => this.addRepository(event);
+  }
+
+  setLoading(loading = true) {
+    if (loading === true) {
+      let loadingEl = document.createElement('span');
+      loadingEl.appendChild(document.createTextNode('Carregando'));
+      loadingEl.setAttribute('id', 'loading');
+
+      this.formEl.appendChild(loadingEl);
+
+    } else {
+      document.getElementById('loading').remove();
+    }
+  }
+
+  async addRepository(event) {
+    event.preventDefault();
+
+    const repoInput = this.inputEl.value;
+
+    if (repoInput.length === 0)
+      return;
+
+    this.setLoading();
+
+    try {
+      const response = await api.get(`/repos/${repoInput}`);
+
+      const { name, description, html_url, owner: { avatar_url } } = response.data;
+
+      console.log(response);
+
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url,
+      });
+
+      this.inputEl.value = '';
+
+      this.render();
+
+    } catch (e) {
+      alert('Repositório não existente!');
+    }
+
+    this.setLoading(false);
+  }
+
+  render() {
+    this.listEl.innerHTML = '';
+
+    this.repositories.forEach(repo => {
+      let imgEl = document.createElement('img');
+      imgEl.setAttribute('src', repo.avatar_url);
+
+      let titleEl = document.createElement('strong');
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement('p');
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement('a');
+      Object.assign(linkEl, {
+        target: '_blank',
+        href: repo.html_url
+      });
+      linkEl.appendChild(document.createTextNode('Acessar'));
+
+      let listItemEl = document.createElement('li');
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+    });
+
+  }
+}
+
+new Repo();
+
 /* async-await */
 // const minhaPromise = () => new Promise((resolve, reject) => {
 //     setTimeout(() => { resolve('OK') }, 2000);
@@ -20,18 +120,18 @@
 // executaPromise();
 
 /* axios */
-import axios from 'axios';
-
-class Api {
-    static async getUserInfo(username) {
-        try {
-            const response = await axios.get(`https://api.github.com/users/${username}`);
-            console.log(response);
-        } catch (err) {
-            console.warn('Erro na req');
-        }
-        
-    }
-}
-
-Api.getUserInfo('rockenbachz');
+// import axios from 'axios';
+//
+// class Api {
+//     static async getUserInfo(username) {
+//         try {
+//             const response = await axios.get(`https://api.github.com/users/${username}`);
+//             console.log(response);
+//         } catch (err) {
+//             console.warn('Erro na req');
+//         }
+//
+//     }
+// }
+//
+// Api.getUserInfo('rockenbachz');
